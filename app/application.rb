34426a7a1d9390ff4122f510e7ddd4c44e7b6aae
Min Kim <mk4555@nyu.edu>
2018-01-12 01:48:41 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,6 +5,7 @@ class Application
 
     if req.path.match(/items/)
       item = req.path.split("/items/").last
+      puts "#{item}"
       if @@items.include?(item)
         resp.status = 200
         resp.write "#{item.price}"
