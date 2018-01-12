3376095d1f0ee39e6f59e3763047a1c6d44cac2e
Min Kim <mk4555@nyu.edu>
2018-01-12 01:58:37 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item = Item.all.find{|i| i.name == item_name}
-      puts "#{item.name}"
+      puts "huh #{item.name}"
       if item != nil
         resp.status = 200
         resp.write "#{item.price}"
